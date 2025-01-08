eb435840d298b40082fcc798bb5dce09562ee0f7
keatongreer <145523241+keatongreer@users.noreply.github.com>
2025-01-08 00:24:08 -0500

    troubleshooting 2

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,10 +1,17 @@
-const db = require("../config/connection.js");
-const Question = require("../models/Question.js");
-const cleanDB = require("./cleanDb.js");
-const pythonQuestions = require('./pythonQuestions.json');
+import fs from 'fs/promises';
+import db from "../config/connection.js";
+import Question from "../models/Question.js";
+import cleanDB from "./cleanDb.js";
+
+async function loadJson(filePath) {
+  const data = await fs.readFile(filePath, 'utf-8');
+  return JSON.parse(data);
+}
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
+
+  const pythonQuestions = await loadJson(new URL('./pythonQuestions.json', import.meta.url).pathname);
 
   await Question.insertMany(pythonQuestions);
 
