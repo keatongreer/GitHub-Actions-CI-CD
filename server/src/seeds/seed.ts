d98408ce6016951ac1de18425f61533fca9f2902
Keaton Greer <keatongreer1@gmail.com>
2025-01-15 20:21:00 -0500

    Fixed ts error in seeds

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,20 +1,22 @@
-import fs from 'fs/promises';
+import fs from "fs/promises";
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-async function loadJson(filePath) {
-  const data = await fs.readFile(filePath, 'utf-8');
+async function loadJson(filePath: string) {
+  const data = await fs.readFile(filePath, "utf-8");
   return JSON.parse(data);
 }
 
-db.once('open', async () => {
-  await cleanDB('Question', 'questions');
+db.once("open", async () => {
+  await cleanDB("Question", "questions");
 
-  const pythonQuestions = await loadJson(new URL('../../src/seeds/pythonQuestions.json', import.meta.url).pathname);
+  const pythonQuestions = await loadJson(
+    new URL("../../src/seeds/pythonQuestions.json", import.meta.url).pathname
+  );
 
   await Question.insertMany(pythonQuestions);
 
-  console.log('Questions seeded!');
+  console.log("Questions seeded!");
   process.exit(0);
 });
